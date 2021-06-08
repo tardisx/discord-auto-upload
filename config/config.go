283e0f35844d82c284d112283e45b99a6c21e8de
Justin Hawkins <justin@hawkins.id.au>
2021-06-08 22:19:37 +0930

    Bump version

--- config/config.go
+++ config/config.go
@@ -22,7 +22,7 @@ var Config struct {
 	Exclude     string
 }
 
-const CurrentVersion string = "0.9"
+const CurrentVersion string = "0.10"
 
 // Load the current config or initialise with defaults
 func LoadOrInit() {
